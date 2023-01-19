1d5fefd267c881d6605082fe046546e1c78bb3fd
Rico Steinke <rico227@gmx.de>
2023-01-19 13:44:58 +0100

    update dataset generation

--- start_training.sh
+++ start_training.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python train.py --workers 8 --device 0 --batch-size 8 --epochs 10 --data dataset/data.yaml --img 640 640 --cfg cfg/training/yolov7.yaml --weights 'yolov7_training.pt' --name yolov7-gtsdb --hyp data/hyp.scratch.custom.yaml
+python train.py --workers 8 --device 0 --batch-size 4 --epochs 500 --data dataset/data.yaml --img 640 640 --cfg cfg/training/yolov7-tfr.yaml --weights 'yolov7_training.pt' --name yolov7-ai --hyp data/hyp.scratch.custom.yaml
